c5ad34ebdcfdd43bf15bfc99be2b9bcc83fcafde
sariasss <sara.arias085@gmail.com>
2024-04-24 13:58:48 +0200

    interfaz PincelCirculo

--- src/main/java/clases/PincelCirculo.java
+++ src/main/java/clases/PincelCirculo.java
@@ -6,6 +6,6 @@ import javafx.scene.canvas.GraphicsContext;
 public interface PincelCirculo extends Pincel{
     public int getRadio();
     public default void dibujar(GraphicsContext g, Punto p){
-        throw new UnsupportedOperationException("No programado");
+        g.fillOval(p.x(), p.y(), this.getRadio(), this.getRadio());
     }
 }
