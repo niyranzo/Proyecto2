cf8209c59f4de8b7281029f0a08d8eccd67a47b1
pacopedrosa <pacopedrosa2018@gmail.com>
2024-04-24 14:42:34 +0200

    Comienzo de clase pincelEstrella y reparto de tareas

--- src/main/java/clases/PincelEstrella.java
+++ src/main/java/clases/PincelEstrella.java
@@ -7,8 +7,8 @@ import java.awt.*;
 public class PincelEstrella implements Pincel{
 
 
-    //private static final Image ESTRELLA =
-
+    //private static final Image ESTRELLA = ClassLoader.getSystemResourceAsStream("");
+    //Poner la ruta para cargar la imagen desde la carpeta de resources
     @Override
     public void dibujar(GraphicsContext g, Punto p) {
 
