a44df7395d149067c495a07b86e971ce3a33a38a
pacopedrosa <pacopedrosa2018@gmail.com>
2024-04-24 14:34:45 +0200

    Comienzo de clase pincelEstrella y reparto de tareas

---
+++ src/main/java/clases/PincelEstrella.java
@@ -0,0 +1,16 @@
+package clases;
+
+import javafx.scene.canvas.GraphicsContext;
+
+import java.awt.*;
+
+public class PincelEstrella implements Pincel{
+
+
+    //private static final Image ESTRELLA =
+
+    @Override
+    public void dibujar(GraphicsContext g, Punto p) {
+
+    }
+}
