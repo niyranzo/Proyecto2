8d1f3cb026d9117ee0ac77b51bd7678f7fd0d6a9
niyranzo <yranzo.nicole@gmail.com>
2024-04-25 10:15:50 +0200

    hasta empezarDibujar

--- src/main/java/controlador/ControladorFrmPaint.java
+++ src/main/java/controlador/ControladorFrmPaint.java
@@ -1,7 +1,10 @@
 package controlador;
 
+import clases.Pincel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
@@ -12,7 +15,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ControladorFrmPaint implements Initializable{
@@ -29,7 +37,7 @@ public class ControladorFrmPaint implements Initializable{
     private Button btnNuevo;
 
     @FXML
-    private ComboBox<?> cmbPinceles;
+    private ComboBox<Pincel> cmbPinceles;
 
     @FXML
     private Canvas cnvLienzo;
@@ -45,6 +53,26 @@ public class ControladorFrmPaint implements Initializable{
         this.btnNuevo.setGraphic(new ImageView(new Image(ClassLoader.getSystemResourceAsStream("Action-file-new-icon.png"))));
         this.imgPinceles=new ImageView(new Image(ClassLoader.getSystemResourceAsStream("Pencil-icon.png")))
     }
+    public void inicializarComboBox(){
+        List<Pincel> pinceles= new ArrayList<>();
+        this.cmbPinceles.getItems().setAll(pinceles);
+        this.cmbPinceles.setValue(pinceles.get(0));
+        this.cmbPinceles.setOnAction(event -> {
+            this.cmbPinceles.getValue();
+        });
+    }
+    //te quedaste aca
+    public void empezarDibujar(){
+
+    }
+    public void nuevaImagen(){
+        GraphicsContext grafico=this.cnvLienzo.getGraphicsContext2D();
+        Paint paint=grafico.getFill();
+        grafico.setFill(Color.WHITE);
+        grafico.fillRect(0,0,this.cnvLienzo.getWidth(), this.cnvLienzo.getHeight());
+        grafico.setFill(paint);
+    }
+
 
     @FXML
     void abrirImagen(ActionEvent event) {
@@ -73,6 +101,8 @@ public class ControladorFrmPaint implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        this.inicializarIconos();
+        this.inicializarComboBox();
+        this.nuevaImagen();
     }
 }
