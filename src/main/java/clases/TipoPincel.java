79bb5b371c5430d52481c745be08f1e48e9f8165
pacopedrosa <pacopedrosa2018@gmail.com>
2024-04-25 10:12:23 +0200

    Enum TipoPincel terminada

--- src/main/java/clases/TipoPincel.java
+++ src/main/java/clases/TipoPincel.java
@@ -1,5 +1,8 @@
 package clases;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum TipoPincel{
     PINCEL_BASICO(new PincelBasico()),PINCEL_GORDO(new PincelGordo()),PINCEL_GROSOR_VARIABLE(new PincelGrosorVariable(2,20)),PINCEL_CONTINUO(new PincelContinuo()),PINCEL_RECTANGULO(new PincelRectangulo()),PINCEL_ESTRELLA(new PincelEstrella());
 
@@ -11,6 +14,16 @@ public enum TipoPincel{
 
     public Pincel getPincel(){
         return this.pincel;
+    }
+
+    public static List<Pincel> getPinceles(){
+
+        List<Pincel> pinceles = new ArrayList<>();
+        for (TipoPincel pincel1:TipoPincel.values()){
+
+            pinceles.add(pincel1.getPincel());
+        }
+        return pinceles;
     }
 
 
