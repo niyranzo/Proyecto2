d7921068e7f17acfcc3be3557cd8f3501550b7f8
pacopedrosa <pacopedrosa2018@gmail.com>
2024-04-25 09:50:52 +0200

    Realizacion de enum

--- src/main/java/clases/TipoPincel.java
+++ src/main/java/clases/TipoPincel.java
@@ -1,4 +1,17 @@
 package clases;
 
-public enum TipoPincel {
+public enum TipoPincel{
+    PINCEL_BASICO(new PincelBasico()),PINCEL_GORDO(new PincelGordo()),PINCEL_GROSOR_VARIABLE(new PincelGrosorVariable(2,20)),PINCEL_CONTINUO(new PincelContinuo()),PINCEL_RECTANGULO(new PincelRectangulo()),PINCEL_ESTRELLA(new PincelEstrella());
+
+    private Pincel pincel;
+
+    private TipoPincel(Pincel p){
+        this.pincel = p;
+    }
+
+    public Pincel getPincel(){
+        return this.pincel;
+    }
+
+
 }
