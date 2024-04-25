ecdb815db59f8094c6d1e9862740f01b4e0b9ab6
niyranzo <yranzo.nicole@gmail.com>
2024-04-25 09:49:47 +0200

    commit necesario

--- src/main/java/controlador/ControladorFrmPaint.java
+++ src/main/java/controlador/ControladorFrmPaint.java
@@ -1,5 +1,78 @@
 package controlador;
 
-public class ControladorFrmPaint {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 
+import javafx.scene.image.ImageView;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorFrmPaint implements Initializable{
+    @FXML
+    private BorderPane Panel;
+
+    @FXML
+    private Button btnAbrir;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private Button btnNuevo;
+
+    @FXML
+    private ComboBox<?> cmbPinceles;
+
+    @FXML
+    private Canvas cnvLienzo;
+
+    @FXML
+    private ColorPicker cpkColor;
+
+    @FXML
+    private ImageView imgPinceles;
+    public void inicializarIconos(){
+        this.btnAbrir.setGraphic(new ImageView(new Image(ClassLoader.getSystemResourceAsStream("open-file-icon.png"))));
+        this.btnGuardar.setGraphic(new ImageView(new Image(ClassLoader.getSystemResourceAsStream("Save-icon.png"))));
+        this.btnNuevo.setGraphic(new ImageView(new Image(ClassLoader.getSystemResourceAsStream("Action-file-new-icon.png"))));
+        this.imgPinceles=new ImageView(new Image(ClassLoader.getSystemResourceAsStream("Pencil-icon.png")))
+    }
+
+    @FXML
+    void abrirImagen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarColor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dibujarPunto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void empezarDibujar(MouseEvent event) {
+
+    }
+
+    @FXML
+    void guardarImagen(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
