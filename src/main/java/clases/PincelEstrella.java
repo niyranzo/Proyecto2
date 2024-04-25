0010b9657fd91b770b179ed3a48c226a557b841b
pacopedrosa <pacopedrosa2018@gmail.com>
2024-04-25 09:05:03 +0200

    Realizacion de clases PincelEstrella y PincelRectangulo

--- src/main/java/clases/PincelEstrella.java
+++ src/main/java/clases/PincelEstrella.java
@@ -3,14 +3,22 @@ package clases;
 import javafx.scene.canvas.GraphicsContext;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class PincelEstrella implements Pincel{
 
 
-    //private static final Image ESTRELLA = ClassLoader.getSystemResourceAsStream("");
+
+    private static final javafx.scene.image.Image ESTRELLA = new javafx.scene.image.Image(ClassLoader.getSystemResourceAsStream("estrella.png"));
     //Poner la ruta para cargar la imagen desde la carpeta de resources
     @Override
     public void dibujar(GraphicsContext g, Punto p) {
+        g.drawImage(ESTRELLA,p.x(),p.y());
 
+    }
+
+    @Override
+    public String toString() {
+        return "Pincel estrella";
     }
 }
